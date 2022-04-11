b5f82c0b8be92401922283212f13b80d821cb00b
Carlos <up201907716@up.pt>
2022-04-11 23:21:25 +0100

    Logic to read from files

---
+++ Proj1/utils.py
@@ -0,0 +1,40 @@
+from formulation import *
+
+def read_dataset(filename):
+  
+  f = open(filename + ".txt", 'r')
+  line = f.readline()
+  tokens = line.split()
+  team = Team(int(tokens.pop(0)), int(tokens.pop(0)))
+
+  #For each member read: member_name member_skills_no
+  for i in range(team.n_members):
+    #
+    line = f.readline()
+    tokens = line.split()
+    member = Member(tokens.pop(0))
+    
+    #For each skill read: skill_name skill_level
+    for k in range(int(tokens.pop(0))):
+      line = f.readline()
+      tokens = line.split()
+      skill = Skill(tokens.pop(0), int(tokens.pop(0)))
+      member.skills.append(skill)
+      
+    team.members.append(member)
+
+  for i in range(team.n_projects):
+    line = f.readline()
+    tokens = line.split()
+    project = Project(tokens.pop(0), int(tokens.pop(0)), int(tokens.pop(0)), int(tokens.pop(0)))
+
+    for k in range (project.n_roles):
+      line = f.readline()
+      tokens = line.split()
+      role = Skill(tokens.pop(0), int(tokens.pop(0)))
+      project.roles.append(role)
+
+    team.projects.append(project)
+
+  return team
+
